df4bc748cccb5cfddaafa8e23110de831a9a5676
wcypersonal <34592158+wcypersonal@users.noreply.github.com>
2018-10-14 19:26:14 +0800

    修改端口

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'transfer/index', to: 'transfer#index'
-  get 'transfer/create/:id', to: 'transfer#create'
-  get 'transfer/update/:id', to: 'transfer#update'
-  get 'transfer/delete/:id', to: 'transfer#delete'
-  get 'transfer/destroy', to: 'transfer#destroy'
+  scope :api do
+    get 'transfer/index', to: 'transfer#index'
+    get 'transfer/create/:id', to: 'transfer#create'
+    get 'transfer/update/:id', to: 'transfer#update'
+    get 'transfer/delete/:id', to: 'transfer#delete'
+    get 'transfer/destroy', to: 'transfer#destroy'
+  end
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
