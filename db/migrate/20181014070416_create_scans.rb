ea9d9f52733a8e4122e5a11e433110192e157acb
sjtudoitit <43645693+sjtudoitit@users.noreply.github.com>
2018-10-16 21:16:16 +0800

    Update 20181014070416_create_scans.rb

--- db/migrate/20181014070416_create_scans.rb
+++ db/migrate/20181014070416_create_scans.rb
@@ -1,14 +1,11 @@
 class CreateScans < ActiveRecord::Migration[5.1]
   def change
     create_table :scans do |t|
-      t.string :_id
       t.string :_openid
       t.string :time
-      t.string :charSet
       t.string :rawData
       t.string :result
-      t.string :scanType
-      t.float :accuracy
+      t.float :alt
       t.float :lat
       t.float :lng
 
